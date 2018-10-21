9cb4d4efcff7e8eb26d706cc5212983522ee3989
tlmooreclemson <tlmoore1928@gmail.com>
2018-10-21 19:06:12 +0200

    Add files via upload

--- napoliWeather.py
+++ napoliWeather.py
@@ -14,7 +14,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 df = pd.read_csv('napoliWeather_2005_2018.csv', sep = ';') # import the data set
 df['Date_Time'] = pd.to_datetime(df['Date_Time'], format = '%d.%m.%Y %H:%M')
+# ========================================================================
 # To find the averages over each day.
-cols = df.columns.difference(['Date_Time'])
-df[cols] = df[cols].astype(str).apply(pd.to_numeric, errors = 'coerce')
-df = df.resample('d', on = 'Date_Time').mean().dropna(how = 'all')
+# cols = df.columns.difference(['Date_Time'])
+# df[cols] = df[cols].astype(str).apply(pd.to_numeric, errors = 'coerce')
+# df = df.resample('d', on = 'Date_Time').mean().dropna(how = 'all')
+# ==========
+# Another way to separate the dates could be done like below. Not sure the best way.
+df['Dates'] = pd.to_datetime(df['Date_Time']).dt.date # creates a 'date only' column
+df_avgTemp = df[['Dates', 'Temp_C']]
+df_avgTemp = df_avgTemp.groupby('Dates')
+df_avgTemp = df_avgTemp.mean()
+# ========================================================================
