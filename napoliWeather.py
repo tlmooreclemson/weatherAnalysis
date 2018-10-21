56e4d00750c9ac19cb3d031f225fc85a29709f55
tlmooreclemson <tlmoore1928@gmail.com>
2018-10-21 18:35:51 +0200

    Add files via upload

---
+++ napoliWeather.py
@@ -0,0 +1,20 @@
+"""
+# =======================================================================
+# Project created on:  21 Oct. 2018
+# Last updated on:     21 Oct. 2018
+# Created by:          Thomas Moore
+# =======================================================================
+# Object of this script is to analyze the meteorological data for Napoli.
+# This data was downloaded from https://rp5.ru
+# =======================================================================
+"""
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+df = pd.read_csv('napoliWeather_2005_2018.csv', sep = ';') # import the data set
+df['Date_Time'] = pd.to_datetime(df['Date_Time'], format = '%d.%m.%Y %H:%M')
+# To find the averages over each day.
+cols = df.columns.difference(['Date_Time'])
+df[cols] = df[cols].astype(str).apply(pd.to_numeric, errors = 'coerce')
+df = df.resample('d', on = 'Date_Time').mean().dropna(how = 'all')
